59568747acb9bb499ea7901fbfba7cffe6645dfd
Matthew Marcus <mmarcus@viprsolutions.com>
2018-12-18 16:57:31 +0000

    Day 5 Part 2

--- test/advent2015_test.exs
+++ test/advent2015_test.exs
@@ -44,4 +44,24 @@ defmodule Advent2015Test do
     assert Advent2015.deliveries2("^>v<") == 3
     assert Advent2015.deliveries2("^v^v^v^v^v") == 11
   end
+
+  test "day 4 part 1" do
+    assert Advent2015.hashproducer("abcdef") == 609043
+    assert Advent2015.hashproducer("pqrstuv") == 1048970
+  end
+
+  test "day 5 part 1" do
+    assert Advent2015.nice?("ugknbfddgicrmopn")
+    assert Advent2015.nice?("aaa")
+    assert not Advent2015.nice?("jchzalrnumimnmhp")
+    assert not Advent2015.nice?("haegwjzuvuyypxyu")
+    assert not Advent2015.nice?("dvszwmarrgswjxmb")
+  end
+
+  test "day 5 part 2" do
+    assert Advent2015.nicer?("qjhvhtzxzqqjkmpb")
+    assert Advent2015.nicer?("xxyxx")
+    assert not Advent2015.nicer?("uurcxstgmygtbstg")
+    assert not Advent2015.nicer?("ieodomkazucvgmuy")
+  end
 end
